8a0c91b11d5c50880c44fca374ffdb3ede559cea
rogerg9999 <rogerg9999@gmail.com>
2013-10-27 23:19:37 +0100

    first

--- app.js
+++ app.js
@@ -21,7 +21,36 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-app.use(robots.crawler);
+app.use(function(req, res, next) {
+  var fragment = req.query._escaped_fragment_;
+
+  // If there is no fragment in the query params
+  // then we're not serving a crawler
+  if (!fragment) return next();
+
+  // If the fragment is empty, serve the
+  // index page
+  if (fragment === "" || fragment === "/")
+    fragment = "/index.html";
+
+  // If fragment does not start with '/'
+  // prepend it to our fragment
+  if (fragment.charAt(0) !== "/")
+    fragment = '/' + fragment;
+
+  // If fragment does not end with '.html'
+  // append it to the fragment
+  if (fragment.indexOf('.html') == -1)
+    fragment += ".html";
+
+  // Serve the static html snapshot
+  try {
+    var file = __dirname + "/snapshots" + fragment;
+    res.sendfile(file);
+  } catch (err) {
+    res.send(404);
+  }
+});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
