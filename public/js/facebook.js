9257dfae9405087aa0d2cdb3daf3d54ca4c1f9ba
rogerg9999 <rogerg9999@gmail.com>
2013-10-30 16:18:10 +0100

    def

---
+++ public/js/facebook.js
@@ -0,0 +1,15 @@
+
+window.fbAsyncInit = function() {
+  FB.init({
+        appId      : '252390838242679',                        // App ID from the app dashboard                            // Check Facebook Login status
+        xfbml      : true                                  // Look for social plugins on the page
+      });
+};
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/es_LA/all.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
